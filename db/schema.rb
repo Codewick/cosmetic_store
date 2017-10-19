7949b19a9eeb2da52410cbf7b484cc7ecdddc021
Sakshi <correo.sakshi@gmail.com>
2017-10-19 15:57:24 +1100

    fix: Add Devise partials to layout view. Integrated duplicate layout file

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171019032534) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,7 +37,6 @@ ActiveRecord::Schema.define(version: 20171019032534) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
