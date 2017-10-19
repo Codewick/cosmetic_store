c7733cb62d576cfb5051ea8191d5eb835e563610
Sakshi <correo.sakshi@gmail.com>
2017-10-19 23:35:19 +1100

    Add admin login feature to devise for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019051432) do
+ActiveRecord::Schema.define(version: 20171019062227) do
 
   create_table "products", force: :cascade do |t|
     t.text "image"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20171019051432) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Inserting default security users
+users = {
+
+    admin: {
+
+        # username: 'admin',
+        email: 'admin@gmail.com',
+        password: 'adminpass',
+        password_confirmation: 'adminpass',
+        admin: true
+    },
+
+    administrator: {
+
+        # username: 'administrator',
+        email: 'administrator@gmail.com',
+        password: 'administrator',
+        password_confirmation: 'administrator',
+        admin: true
+    }
+}
+
+users.each do |user, data|
+
+  user = User.new(data)
+
+  unless User.where(email: user.email).exists?
+    user.save!
+  end
+end
