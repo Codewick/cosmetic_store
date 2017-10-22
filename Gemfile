114302c4ebb36fa1dd23ff379ee74e74eadee46c
Reena Patil <reena.patil@outlook.com>
2017-10-22 19:58:23 +1100

    modified product show page

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'bootstrap', '~> 4.0.0.beta'
 gem 'jquery-rails'
 
 gem 'devise'
+gem 'font-awesome-sass'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020034756) do
+ActiveRecord::Schema.define(version: 20171022074107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20171020034756) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "profiles", force: :cascade do |t|
